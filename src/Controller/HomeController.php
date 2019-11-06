8dcec41b3f4a8e62bc771afd90f80fce31cf362e
Raimondas Ražinskis <feknaz@gmail.com>
2019-11-07 01:46:27 +0200

    fixed HomeController

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Academy;
-use App\Entity\AcademyType;
-use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,17 +10,8 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(EntityManagerInterface $entityManager)
+    public function index()
     {
-        $academy = new Academy();
-        $academy->setTitle('Nauja profke');
-        $academy->setAcademyType(AcademyType::technical());
-
-        $entityManager->persist($academy);
-        $entityManager->flush();
-
-        dump($academy->getAcademyType());
-
         return $this->render('home/index.html.twig', [
             'someVariable' => 'NFQ Akademija test',
         ]);
