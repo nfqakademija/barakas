54a2d1f98cf52627225f153a594ebd08739d739b
Mantas Dobrodiejus <dobrodiejusmantas@gmail.com>
2019-12-10 21:22:53 +0200

    Saving

--- assets/js/realTimeMessages.js
+++ assets/js/realTimeMessages.js
@@ -2,5 +2,7 @@ const u = new URL('http://127.0.0.1:9090/.well-known/mercure');
 u.searchParams.append('topic', window.location.href);
 const es = new EventSource(u);
 es.onmessage = e => {
+    const ul = document.getElementById("message");
     console.log(JSON.parse(e.data));
 }
+
